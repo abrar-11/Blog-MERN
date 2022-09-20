3795d208c9949cc32dcd71a61e9c70f75df9c49e
abar ahmed <abrarahmed2185@gmail.com>
2022-09-21 00:21:54 +0500

    CRUD

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,17 +1,61 @@
-const getPosts = (req, res) => {
-   res.status(200).json({ success: true, data: "Get ALl Posts" });
+import mongoose from "mongoose";
+import Posts from "../models/Posts.js";
+
+const getPosts = async (req, res) => {
+   try {
+      const posts = await Posts.find();
+      res.status(200).json({ success: true, data: posts });
+   } catch (error) {
+      res.status(500).json({ success: false, error: error });
+   }
 };
 
 const getPost = (req, res) => {
    res.status(200).json({ success: true, data: "Get Get Post" });
 };
 
-const createPost = (req, res) => {
-   res.status(200).json({ success: true, data: "Get Create Posts" });
+const createPost = async (req, res) => {
+   try {
+      const postData = req.body;
+      console.log(postData);
+      if (postData) {
+         const newPost = new Posts(postData);
+         await newPost.save();
+
+         res.status(200).json({ success: true, data: newPost });
+      }
+   } catch (error) {
+      res.status(500).json({ success: false, error: error.message });
+   }
 };
 
-const updatePost = (req, res) => {
-   res.status(200).json({ success: true, data: "Get Update Posts" });
+const updatePost = async (req, res) => {
+   try {
+      const postData = req.body;
+      const id = req.params.id;
+      console.log(postData && id);
+
+      if (!(id && mongoose.isValidObjectId(id)))
+         return res
+            .status(500)
+            .json({ success: false, error: "Not a valid Id" });
+
+      const getPost = await Posts.findById(id);
+
+      if (!getPost)
+         return res
+            .status(500)
+            .json({ success: false, error: "Id Not found.." });
+
+      const newPost = await Posts.findByIdAndUpdate(id, postData, {
+         runValidators: true,
+         new: true,
+      });
+
+      res.status(200).json({ success: true, data: newPost });
+   } catch (error) {
+      res.status(500).json({ success: false, error: error.message });
+   }
 };
 const deletePost = (req, res) => {
    res.status(200).json({ success: true, data: "Get Delete Posts" });
--- server/server.js
+++ server/server.js
@@ -3,14 +3,19 @@ import dotenv from "dotenv";
 import colors from "colors";
 import postRouter from "./routes/postRouter.js";
 import connectDB from "./config/database.js";
-
 dotenv.config();
+
+
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 // Connecting to Database
 connectDB();
+
+app.use(express.json());
+
 app.use("/api/v1", postRouter);
 
 app.listen(PORT, () => {
